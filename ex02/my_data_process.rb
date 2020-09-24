27113c8314b443a987d6a2f40f8f8bac2c26b112
ibrahim bashir <bashir_i@qwasar.io>
2020-09-24 14:19:51 +0000

    refactored the code

--- ex02/my_data_process.rb
+++ ex02/my_data_process.rb
@@ -1,49 +1,10 @@
 require 'date'
 require 'json'
 
-def get_time(date_string)
-    hour = DateTime.parse(date_string, '%Y-%m-%d %H:%M:%S').hour
-    return hour >= 18 && hour < 24 ? 'evening' : hour >= 12 && hour < 18 ? 'afternoon' : 'morning'
-end
-
 def my_csv_parser(param_1, param_2 = ",")
     param_1.split("\n").map! do |row|
         row.split(param_2)
     end
-end
-
-def get_age_range(age)
-    if age > 1 && age <= 20
-        return "1->20"
-    elsif age > 20 && age <= 40
-        return "21->40"
-    elsif age > 40 && age <= 65
-         return "41->65"
-    elsif age > 65 && age < 100
-         return "66->99"
-    else
-        return "Invalid range"
-    end
-end
-
-def get_domain(email)
-    return email.split("@").last
-end
-
-def my_data_transform(data)
-    info = my_csv_parser(data)
-    transformed = []
-    transformed << info.first
-    info.each_with_index do |line, i|
-        next if i == 0
-        line.each_with_index do |record, index|
-            index == 4 && line[index] = get_domain(line[index])
-            index == 5 && line[index] = get_age_range(line[index].to_i)
-        end
-        line[-1] = get_time(line[-1])
-        transformed << line
-    end
-    return transformed.map! { |line| line.join(",") }.join("\n") + '\n'
 end
 
 def my_data_process(csv_string)
@@ -76,7 +37,5 @@ def my_data_process(csv_string)
     return results.to_json
 end
 
-# sample = "Gender,FirstName,LastName,UserName,Email,Age,City,Device,Coffee Quantity,Order At\nMale,Carl,Wilderman,carl,wilderman_carl@yahoo.com,53,Abuja,Safari iPhone,2,2020-03-06 16:37:56\nFemale,Marvin,Lind,marvin,marvin_lind@gmail.com,77,Detroit,Chrome Android,2,2020-03-02 8:55:51\nFemale,Shanelle,Marquardt,shanelle,marquardt.shanelle@hotmail.com,21,Las Vegas,Chrome,1,2020-03-05 21:53:05\n"
-# output = my_data_transform(sample)
-# transformed_output =  my_data_process(output)
+# transformed_output =  my_data_process("Gender,FirstName,LastName,UserName,Email,Age,City,Device,Coffee Quantity,Order At\nMale,Carl,Wilderman,carl,yahoo.com,21->40,Seattle,Safari iPhone,2,afternoon\nMale,Marvin,Lind,marvin,hotmail.com,66->99,Detroit,Chrome Android,2,afternoon\nFemale,Shanelle,Marquardt,shanelle,hotmail.com,21->40,Las Vegas,Chrome,1,afternoon\nFemale,Lavonne,Romaguera,lavonne,yahoo.com,66->99,Seattle,Chrome,2,morning\nMale,Derick,McLaughlin,derick,hotmail.com,41->65,Chicago,Chrome Android,1,afternoon")
 # puts transformed_output
