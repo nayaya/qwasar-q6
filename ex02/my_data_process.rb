a0e909cc3a9f48195ca0268640c6d0a05618f34f
ibrahim bashir <bashir_i@qwasar.io>
2020-09-24 15:34:50 +0000

    commented helper csv parser

--- ex02/my_data_process.rb
+++ ex02/my_data_process.rb
@@ -1,14 +1,17 @@
 require 'date'
 require 'json'
 
-def my_csv_parser(param_1, param_2 = ",")
-    param_1.split("\n").map! do |row|
-        row.split(param_2)
-    end
-end
+# def my_csv_parser(param_1, param_2 = ",")
+#     param_1.split("\n").map! do |row|
+#         row.split(param_2)
+#     end
+# end
 
 def my_data_process(param_1)
-    array = my_csv_parser(param_1)
+    array = param_1.split("\n")
+    array.map! do |row|
+        row.split(",")
+    end
     headers = array.first
     head_vals = Hash.new
     values = []
@@ -37,5 +40,5 @@ def my_data_process(param_1)
     return results.to_json
 end
 
-# transformed_output =  my_data_process("Gender,FirstName,LastName,UserName,Email,Age,City,Device,Coffee Quantity,Order At\nMale,Carl,Wilderman,carl,yahoo.com,21->40,Seattle,Safari iPhone,2,afternoon\nMale,Marvin,Lind,marvin,hotmail.com,66->99,Detroit,Chrome Android,2,afternoon\nFemale,Shanelle,Marquardt,shanelle,hotmail.com,21->40,Las Vegas,Chrome,1,afternoon\nFemale,Lavonne,Romaguera,lavonne,yahoo.com,66->99,Seattle,Chrome,2,morning\nMale,Derick,McLaughlin,derick,hotmail.com,41->65,Chicago,Chrome Android,1,afternoon")
-# print transformed_output
+transformed_output =  my_data_process("Gender,FirstName,LastName,UserName,Email,Age,City,Device,Coffee Quantity,Order At\nMale,Carl,Wilderman,carl,yahoo.com,21->40,Seattle,Safari iPhone,2,afternoon\nMale,Marvin,Lind,marvin,hotmail.com,66->99,Detroit,Chrome Android,2,afternoon\nFemale,Shanelle,Marquardt,shanelle,hotmail.com,21->40,Las Vegas,Chrome,1,afternoon\nFemale,Lavonne,Romaguera,lavonne,yahoo.com,66->99,Seattle,Chrome,2,morning\nMale,Derick,McLaughlin,derick,hotmail.com,41->65,Chicago,Chrome Android,1,afternoon")
+print transformed_output
