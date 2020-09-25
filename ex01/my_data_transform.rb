4efef9f8d9da3918ab0a5e142535c3409e1e2658
ibrahim bashir <bashir_i@qwasar.io>
2020-09-25 09:07:41 +0000

    Reformatted the function return value (ex01) and the param for (ex02 function)

--- ex01/my_data_transform.rb
+++ ex01/my_data_transform.rb
@@ -39,7 +39,7 @@ def my_data_transform(csv_content)
         line[-1] = get_time(line[-1])
         transformed << line
     end
-    return transformed.map! { |line| line.join(",") }.join("\n")
+    return transformed.map! { |line| line.join(",") }
 end
 
-# puts my_data_transform("Gender,FirstName,LastName,UserName,Email,Age,City,Device,Coffee Quantity,Order At\nMale,Carl,Wilderman,carl,wilderman_carl@yahoo.com,29,Seattle,Safari iPhone,2,2020-03-06 16:37:56\nMale,Marvin,Lind,marvin,marvin_lind@hotmail.com,77,Detroit,Chrome Android,2,2020-03-02 13:55:51\nFemale,Shanelle,Marquardt,shanelle,marquardt.shanelle@hotmail.com,21,Las Vegas,Chrome,1,2020-03-05 17:53:05\nFemale,Lavonne,Romaguera,lavonne,romaguera.lavonne@yahoo.com,81,Seattle,Chrome,2,2020-03-04 10:33:53\nMale,Derick,McLaughlin,derick,mclaughlin.derick@hotmail.com,47,Chicago,Chrome Android,1,2020-03-05 15:19:48\n")
+# p my_data_transform("Gender,FirstName,LastName,UserName,Email,Age,City,Device,Coffee Quantity,Order At\nMale,Carl,Wilderman,carl,wilderman_carl@yahoo.com,29,Seattle,Safari iPhone,2,2020-03-06 16:37:56\nMale,Marvin,Lind,marvin,marvin_lind@hotmail.com,77,Detroit,Chrome Android,2,2020-03-02 13:55:51\nFemale,Shanelle,Marquardt,shanelle,marquardt.shanelle@hotmail.com,21,Las Vegas,Chrome,1,2020-03-05 17:53:05\nFemale,Lavonne,Romaguera,lavonne,romaguera.lavonne@yahoo.com,81,Seattle,Chrome,2,2020-03-04 10:33:53\nMale,Derick,McLaughlin,derick,mclaughlin.derick@hotmail.com,47,Chicago,Chrome Android,1,2020-03-05 15:19:48\n")
--- ex02/my_data_process.rb
+++ ex02/my_data_process.rb
@@ -1,16 +1,17 @@
 require 'json'
 
-def my_data_process(param_1)
-    array = param_1.split("\n")
-    array.map! do |row|
+def my_data_process(data_array)
+
+    data_array.map! do |row|
         row.split(",")
     end
-    headers = array.first
+
+    headers = data_array.first
     head_vals = Hash.new
     values = []
     headers.each_with_index do |header, index|
         next if [1,2,3,8].include?(index)
-        array.each_with_index do |record, i|
+        data_array.each_with_index do |record, i|
             next if i == 0
             values << record[index].gsub('\\n', '')
         end 
@@ -33,5 +34,5 @@ def my_data_process(param_1)
     return results.to_json
 end
 
-# transformed_output =  my_data_process("Gender,FirstName,LastName,UserName,Email,Age,City,Device,Coffee Quantity,Order At\nMale,Carl,Wilderman,carl,yahoo.com,21->40,Seattle,Safari iPhone,2,afternoon\nMale,Marvin,Lind,marvin,hotmail.com,66->99,Detroit,Chrome Android,2,afternoon\nFemale,Shanelle,Marquardt,shanelle,hotmail.com,21->40,Las Vegas,Chrome,1,afternoon\nFemale,Lavonne,Romaguera,lavonne,yahoo.com,66->99,Seattle,Chrome,2,morning\nMale,Derick,McLaughlin,derick,hotmail.com,41->65,Chicago,Chrome Android,1,afternoon")
+# transformed_output =  my_data_process(["Gender,FirstName,LastName,UserName,Email,Age,City,Device,Coffee Quantity,Order At", "Male,Carl,Wilderman,carl,yahoo.com,21->40,Seattle,Safari iPhone,2,afternoon", "Male,Marvin,Lind,marvin,hotmail.com,66->99,Detroit,Chrome Android,2,afternoon", "Female,Shanelle,Marquardt,shanelle,hotmail.com,21->40,Las Vegas,Chrome,1,afternoon", "Female,Lavonne,Romaguera,lavonne,yahoo.com,66->99,Seattle,Chrome,2,morning", "Male,Derick,McLaughlin,derick,hotmail.com,41->65,Chicago,Chrome Android,1,afternoon"])
 # print transformed_output
